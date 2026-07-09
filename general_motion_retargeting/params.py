918a4213a3f3b645d5efce287a4ed3a5334482a6
hhebb <igotaspot426@gmail.com>
2026-07-09 17:01:45 +0900

    improve bvh lafan retargeting cfg

--- general_motion_retargeting/params.py
+++ general_motion_retargeting/params.py
@@ -55,7 +55,7 @@ IK_CONFIG_DICT = {
         "fourier_n1": IK_CONFIG_ROOT / "bvh_to_n1.json",
         "stanford_toddy": IK_CONFIG_ROOT / "bvh_to_toddy.json",
         "engineai_pm01": IK_CONFIG_ROOT / "bvh_to_pm01.json",
-        "robros_igris_c_v2": IK_CONFIG_ROOT / "bvh_lafan1_to_igris_c_kist.json",
+        "robros_igris_c_v2": IK_CONFIG_ROOT / "bvh_lafan1_to_igris_c.json",
     },
     "fbx":{
         "unitree_g1": IK_CONFIG_ROOT / "fbx_to_g1.json",
