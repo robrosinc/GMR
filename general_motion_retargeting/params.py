0c43e4bbb914e91ea98578026620ca316f699935
hhebb <igotaspot426@gmail.com>
2026-07-14 12:07:37 +0900

    cfg update

--- general_motion_retargeting/params.py
+++ general_motion_retargeting/params.py
@@ -45,7 +45,7 @@ IK_CONFIG_DICT = {
         "booster_k1": IK_CONFIG_ROOT / "smplx_to_k1.json",
         "pnd_adam_lite": IK_CONFIG_ROOT / "smplx_to_adam.json",
         "tienkung": IK_CONFIG_ROOT / "smplx_to_tienkung.json",
-        "robros_igris_c_v2": IK_CONFIG_ROOT / "smplx_to_igris_c_tmp.json", # smplx_to_igris_c_2, smplx_to_igris_c_tmp, smplx_to_igris_c_kimodo
+        "robros_igris_c_v2": IK_CONFIG_ROOT / "smplx_to_igris_c.json", # smplx_to_igris_c_2, smplx_to_igris_c_tmp, smplx_to_igris_c_kimodo
         "robros_igris_max": IK_CONFIG_ROOT / "smplx_to_igris_max.json",
     },
     "bvh_lafan1":{
--- scripts/retarget_pico_offline.sh
+++ scripts/retarget_pico_offline.sh
@@ -1,7 +1,14 @@
-python scripts/pico_offline_to_robot.py \
-  --raw_pico_dir pico/tmp \
-  --output_dir pico/offline_retargeted \
-  --robot robros_igris_c_v2 \
-  --actual_human_height 1.5 \
-  --motion_fps 100 \
-  --overwrite
+# python scripts/pico_offline_to_robot.py \
+#   --raw_pico_dir pico/offline_turn \
+#   --output_dir pico/offline_turn_retargeted \
+#   --robot robros_igris_c_v2 \
+#   --actual_human_height 1.5 \
+#   --motion_fps 100 \
+#   --overwrite
+
+# igris c
+python scripts/vis_robot_motion_directory.py \
+--robot robros_igris_c_v2 \
+--robot_motion_dir /home/robros/workspace/GMR/pico/offline_turn_retargeted/ \
+--root_quat_scalar_first true
+
